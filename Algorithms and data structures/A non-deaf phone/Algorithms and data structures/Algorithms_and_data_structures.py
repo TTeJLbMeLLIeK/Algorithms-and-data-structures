43fc7925b64ce428d7d068068f2c6c64d23f92ea
TTeJLbMeLLIeK <115335823+TTeJLbMeLLIeK@users.noreply.github.com>
2024-10-19 14:06:49 +0300

    Delete Algorithms and data structures directory

--- Algorithms and data structures/A non-deaf phone/Algorithms and data structures/Algorithms_and_data_structures.py
+++
@@ -1,6 +0,0 @@
-with open("INPUT.TXT", "r") as input_file:
-    number = input_file.read().strip()
-
-with open("OUTPUT.TXT", "w") as output_file:
-    output_file.write(number)
-
--- Algorithms and data structures/A+B/A+B/A_B.py
+++
@@ -1,8 +0,0 @@
-with open("INPUT.TXT", "r") as input_file:
-    a, b = map(int, input_file.read().split())
-
-result = a + b
-
-with open("OUTPUT.TXT", "w") as output_file:
-    output_file.write(str(result))
-
