dff2c563cca550b855d03bc02e7ba08d417346e2
TTeJLbMeLLIeK <115335823+TTeJLbMeLLIeK@users.noreply.github.com>
2024-10-27 15:54:09 +0300

    Add files via upload

---
+++ Homework 3/Homework 3/Speaking.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework_3
+{
+    public interface Speaking
+    {
+        int FindElement(int element);
+        void RemoveElement(int element);
+        int GetArrayLength();
+        int FindMin();
+        int FindMax();
+    }
+}
