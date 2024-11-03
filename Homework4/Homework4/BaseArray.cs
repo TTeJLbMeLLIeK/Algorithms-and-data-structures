1cbe91e2d9840db6331347b602cd83efaa5a144a
TTeJLbMeLLIeK <115335823+TTeJLbMeLLIeK@users.noreply.github.com>
2024-11-03 14:16:30 +0300

    Add files via upload

---
+++ Homework4/Homework4/BaseArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework4
+{
+    public abstract class BaseArray : IArrayOperations
+    {
+        protected int[] items;
+        protected int count;
+
+        public BaseArray(int capacity)
+        {
+            items = new int[capacity];
+            count = 0;
+        }
+
+        public int Count => count;
+
+        public abstract void Insert(int value);
+        public abstract void Remove(int value);
+        public abstract int[] GetAllElements();
+
+        public void PrintElements()
+        {
+            Console.WriteLine("Элементы массива:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(items[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
